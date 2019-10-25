be40bdbac2bb69fc6e9c79b03ef0676bcbbc8a9c
dejongbrian <bochieng25@gmail.com>
2019-10-25 09:24:53 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes';
-}
+  quote = [];
+
+
+  constructor(){
+    this.quote =['On a mission your worst enemy is idle time.  Nipsey Hussle','Only I can change my life. Dejongbrian'
+  ]
+  }
+  }
